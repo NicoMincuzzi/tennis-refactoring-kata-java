64ad0dab31f3022e8078e888e16d1191ffdc6792
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-05-05 09:35:38 +0200

    [refactor]: Improve code readability extarcting static map

--- src/main/java/kata/TennisGame.java
+++ src/main/java/kata/TennisGame.java
@@ -1,11 +1,26 @@
 package kata;
 
+import java.util.Map;
+
 public class TennisGame {
-    
+
+    public static final int DEFAULT_SCORE = -1;
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
+
+    private final Map<Integer, String> scoreMap = Map.of(
+            0, "Love-All",
+            1, "Fifteen-All",
+            2, "Thirty-All",
+            -1, "Deuce");
+
+    private final Map<Integer, String> scoreMap_2 = Map.of(
+            0, "Love",
+            1, "Fifteen",
+            2, "Thirty",
+            3, "Forty");
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -13,7 +28,7 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             m_score1 += 1;
         else
             m_score2 += 1;
@@ -21,56 +36,35 @@ public class TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        int tempScore;
+
+        if (m_score1 == m_score2) {
+            if (scoreMap.get(m_score1) != null)
+                return scoreMap.get(m_score1);
+            return scoreMap.get(DEFAULT_SCORE);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+
+        if (m_score1 >= 4 || m_score2 >= 4) {
+            int minusResult = m_score1 - m_score2;
+            if (minusResult == 1)
+                score = "Advantage player1";
+            else if (minusResult == -1)
+                score = "Advantage player2";
+            else if (minusResult >= 2)
+                score = "Win for player1";
+            else
+                score = "Win for player2";
+            return score;
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+
+        for (int i = 1; i < 3; i++) {
+            if (i == 1)
+                tempScore = m_score1;
+            else {
+                score += "-";
+                tempScore = m_score2;
             }
+            score = score.concat(scoreMap_2.get(tempScore));
         }
         return score;
     }
