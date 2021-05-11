6e0e7bd647c104ead2b0a78a64ed7a48d410b256
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-05-11 09:00:06 +0200

    [refactor]: Extract code in private methods

--- REFACTORING_STEPS.md
+++ REFACTORING_STEPS.md
@@ -1,6 +1,10 @@
-# Refactoring 
+# Refactoring
 
 1. Check IDE's warning
 2. Resolve IDE's warning
 3. Move if-else to static Map
 4. Extract known constant
+5. Extract private method and change naming to advantageScore
+6. Using return in if-else removing score variable. Remove if-else leaving if
+7. Extract private method and change naming to gameScore
+8. return "Deuce" inline
--- src/main/java/kata/TennisGame.java
+++ src/main/java/kata/TennisGame.java
@@ -4,27 +4,22 @@ import java.util.Map;
 
 public class TennisGame {
 
-    public static final int DEFAULT_SCORE = -1;
+    public static final int MAX_POINT_NUMBER = 4;
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private final String player1Name;
-    private final String player2Name;
 
     private final Map<Integer, String> scoreMap = Map.of(
             0, "Love-All",
             1, "Fifteen-All",
-            2, "Thirty-All",
-            -1, "Deuce");
+            2, "Thirty-All");
 
-    private final Map<Integer, String> scoreMap_2 = Map.of(
+    private final Map<Integer, String> gameScoreMap = Map.of(
             0, "Love",
             1, "Fifteen",
             2, "Thirty",
             3, "Forty");
 
     public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
@@ -35,33 +30,37 @@ public class TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore;
-
         if (m_score1 == m_score2) {
             if (scoreMap.get(m_score1) != null)
                 return scoreMap.get(m_score1);
-            return scoreMap.get(DEFAULT_SCORE);
+            return "Deuce";
         }
 
-        if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1)
-                score = "Advantage player1";
-            else if (minusResult == -1)
-                score = "Advantage player2";
-            else if (minusResult >= 2)
-                score = "Win for player1";
-            else
-                score = "Win for player2";
-            return score;
+        if (m_score1 >= MAX_POINT_NUMBER || m_score2 >= MAX_POINT_NUMBER) {
+            return advantageScore();
         }
 
+        return gameScore();
+    }
+
+    private String advantageScore() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1)
+            return "Advantage player1";
+        if (minusResult == -1)
+            return "Advantage player2";
+        if (minusResult >= 2)
+            return "Win for player1";
+        return "Win for player2";
+    }
+
+    private String gameScore() {
+        String score = "";
         for (int i = 1; i < 3; i++) {
             if (i != 1)
                 score = score.concat("-");
-            tempScore = (i == 1) ? m_score1 : m_score2;
-            score = score.concat(scoreMap_2.get(tempScore));
+            int tempScore = (i == 1) ? m_score1 : m_score2;
+            score = score.concat(gameScoreMap.get(tempScore));
         }
         return score;
     }
