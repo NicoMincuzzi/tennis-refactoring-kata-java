6d4f403f53c9800023fd901c056c1e391bb43a3e
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-05-17 09:39:53 +0200

    [feat]: Handle constructor params in wonPoint method

--- src/main/java/kata/TennisGame.java
+++ src/main/java/kata/TennisGame.java
@@ -8,6 +8,9 @@ public class TennisGame {
     private int m_score1 = 0;
     private int m_score2 = 0;
 
+    private final String player1Name;
+    private final String player2Name;
+
     private final Map<Integer, String> scoreMap = Map.of(
             0, "Love-All",
             1, "Fifteen-All",
@@ -20,12 +23,14 @@ public class TennisGame {
             3, "Forty");
 
     public TennisGame(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
+        if (playerName.equals(player1Name))
             m_score1 += 1;
-        else
+        if (playerName.equals(player2Name))
             m_score2 += 1;
     }
 
